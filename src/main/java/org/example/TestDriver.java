3a6045d3140eead0609bd786ec354b3fb863e9ef
kumar saurav <ksaurav.cruise@gmail.com>
2023-06-03 20:36:59 +0530

    Better.com code example

--- src/main/java/org/example/TestDriver.java
+++ src/main/java/org/example/TestDriver.java
@@ -11,31 +11,27 @@ import java.util.*;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class TestDriver {
     @SneakyThrows
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
         String input = Files.lines(Paths.get(args[0])).
                 collect(Collectors.joining("\n"));
-        //array contain all test cases it assumes test are seperated by blank line
+        //array contain all test cases, it assumes test are separated by blank line
         String[] inputArr = input.split("\n\n");
         StringBuilder sb = new StringBuilder();
         //run for each cases
         for (int i = 0; i < inputArr.length; i++) {
-            Queue<DataPoint> dataPointQueue = new LinkedBlockingDeque<>(1024);
+            Queue<DataPoint> dataPointQueue = new LinkedBlockingDeque<>();
+            //extract each test case and their input output node
             String[] singleTestcase = inputArr[i].split("\n");
             String testNumber = singleTestcase[0];
-            sb.append(testNumber);
-            System.out.println("Test number Executing--> " + testNumber);
             Map<DataPoint, Task> inputDataPointToTaskMap = new HashMap<>();
             Integer noOfTasks = Integer.parseInt(singleTestcase[1]);
             boolean[] allExecutedTasks = new boolean[noOfTasks];
+            sb.append(testNumber);
 
             for (int j = 0; j < noOfTasks; j++) {
+                //create a task with input output data nodes
                 String[] inputOutputDataPoints = singleTestcase[j + 2].split(";");
                 List<DataPoint> inputDataNode = Arrays.stream(inputOutputDataPoints[0].split(",")).
                         map(DataPoint::new)
@@ -43,16 +39,17 @@ public class TestDriver {
                 List<DataPoint> outPutDataNode = Arrays.stream(inputOutputDataPoints[1].split(","))
                         .map(DataPoint::new)
                         .collect(Collectors.toList());
-                System.out.println(inputDataNode + "-->" + outPutDataNode);
                 Task task = new Task(j, inputDataNode, outPutDataNode, dataPointQueue, allExecutedTasks);
                 inputDataPointToTaskMap.putAll(
                         inputDataNode.stream()
                                 .collect(Collectors.toMap(Function.identity(), a -> task)));
             }
+            //initial trigger condition
             Arrays.stream(singleTestcase[singleTestcase.length - 1].split(","))
                     .map(DataPoint::new)
                     .forEach(dataPointQueue::add);
             Set<Integer> allExecutedTask = new TreeSet<>();
+            //use BFS to calculate all visited nodes
             while (!dataPointQueue.isEmpty()) {
                 Task executingTask = inputDataPointToTaskMap.get(dataPointQueue.poll());
                 if (Objects.nonNull(executingTask) && !allExecutedTasks[executingTask.getId()]) {
