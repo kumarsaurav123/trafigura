ca360effe77dfe734f33eaf3a2b81e99a977fa6e
kumar saurav <ksaurav.cruise@gmail.com>
2023-06-03 21:05:01 +0530

    Better.com code example

--- src/main/java/org/example/TestDriver.java
+++ src/main/java/org/example/TestDriver.java
@@ -17,54 +17,56 @@ public class TestDriver {
         String input = Files.lines(Paths.get(args[0])).
                 collect(Collectors.joining("\n"));
         //array contain all test cases, it assumes test are separated by blank line
-        String[] inputArr = input.split("\n\n");
-        StringBuilder sb = new StringBuilder();
-        //run for each cases
-        for (int i = 0; i < inputArr.length; i++) {
-            Queue<DataPoint> dataPointQueue = new LinkedBlockingDeque<>();
-            //extract each test case and their input output node
-            String[] singleTestcase = inputArr[i].split("\n");
-            String testNumber = singleTestcase[0];
-            Map<DataPoint, Task> inputDataPointToTaskMap = new HashMap<>();
-            Integer noOfTasks = Integer.parseInt(singleTestcase[1]);
-            boolean[] allExecutedTasks = new boolean[noOfTasks];
-            sb.append(testNumber);
-
-            for (int j = 0; j < noOfTasks; j++) {
-                //create a task with input output data nodes
-                String[] inputOutputDataPoints = singleTestcase[j + 2].split(";");
-                List<DataPoint> inputDataNode = Arrays.stream(inputOutputDataPoints[0].split(",")).
-                        map(DataPoint::new)
-                        .collect(Collectors.toList());
-                List<DataPoint> outPutDataNode = Arrays.stream(inputOutputDataPoints[1].split(","))
-                        .map(DataPoint::new)
-                        .collect(Collectors.toList());
-                Task task = new Task(j, inputDataNode, outPutDataNode, dataPointQueue, allExecutedTasks);
-                inputDataPointToTaskMap.putAll(
-                        inputDataNode.stream()
-                                .collect(Collectors.toMap(Function.identity(), a -> task)));
-            }
-            //initial trigger condition
-            Arrays.stream(singleTestcase[singleTestcase.length - 1].split(","))
-                    .map(DataPoint::new)
-                    .forEach(dataPointQueue::add);
-            Set<Integer> allExecutedTask = new TreeSet<>();
-            //use BFS to calculate all visited nodes
-            while (!dataPointQueue.isEmpty()) {
-                Task executingTask = inputDataPointToTaskMap.get(dataPointQueue.poll());
-                if (Objects.nonNull(executingTask) && !allExecutedTasks[executingTask.getId()]) {
-                    allExecutedTask.add(executingTask.getId());
-                    allExecutedTasks[executingTask.getId()] = true;
-                    executingTask.trigger();
-                }
-            }
-            sb.append(allExecutedTask.stream().map(Object::toString).collect(Collectors.joining(",")));
-            sb.append("\n");
-        }
-        System.out.println(sb.toString());
+       String output =Arrays.stream(input.split("\n\n"))
+                .map(TestDriver::runEachTestCase)
+                .collect(Collectors.joining("\n"));
         Path outputPath = Paths.get(Paths.get(args[0]).getParent().toFile().getPath(), "output.txt");
-        System.out.println("Output Path is" + outputPath.toString());
-        Files.write(outputPath, sb.toString().getBytes(StandardCharsets.UTF_8)
+        System.out.println(output);
+        System.out.println("Output Path is " + outputPath);
+        Files.write(outputPath, output.getBytes(StandardCharsets.UTF_8)
                 , StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.WRITE, StandardOpenOption.CREATE);
+    }
+
+    private static String  runEachTestCase( String testCaseString) {
+        StringBuilder sb = new StringBuilder();
+        Queue<DataPoint> changedDataPointQueue = new LinkedBlockingDeque<>();
+        //extract each test case and their input output node
+        String[] testCase = testCaseString.split("\n");
+        String testNumber = testCase[0];
+        Map<DataPoint, Task> inputDataPointToTaskMap = new HashMap<>();
+        int noOfTasks = Integer.parseInt(testCase[1]);
+        boolean[] allExecutedTasks = new boolean[noOfTasks];
+        sb.append(testNumber);
+
+        for (int j = 0; j < noOfTasks; j++) {
+            //create a task with input output data nodes
+            String[] inputOutputDataPoints = testCase[j + 2].split(";");
+            List<DataPoint> inputDataNode = Arrays.stream(inputOutputDataPoints[0].split(",")).
+                    map(DataPoint::new)
+                    .collect(Collectors.toList());
+            List<DataPoint> outPutDataNode = Arrays.stream(inputOutputDataPoints[1].split(","))
+                    .map(DataPoint::new)
+                    .collect(Collectors.toList());
+            Task task = new Task(j, inputDataNode, outPutDataNode, changedDataPointQueue, allExecutedTasks);
+            inputDataPointToTaskMap.putAll(
+                    inputDataNode.stream()
+                            .collect(Collectors.toMap(Function.identity(), a -> task)));
+        }
+        //initial trigger condition
+        Arrays.stream(testCase[testCase.length - 1].split(","))
+                .map(DataPoint::new)
+                .forEach(changedDataPointQueue::add);
+        Set<Integer> allExecutedTask = new TreeSet<>();
+        //use BFS to calculate all visited nodes
+        while (!changedDataPointQueue.isEmpty()) {
+            Task executingTask = inputDataPointToTaskMap.get(changedDataPointQueue.poll());
+            if (Objects.nonNull(executingTask) && !allExecutedTasks[executingTask.getId()]) {
+                allExecutedTask.add(executingTask.getId());
+                allExecutedTasks[executingTask.getId()] = true;
+                executingTask.trigger();
+            }
+        }
+        sb.append(allExecutedTask.stream().map(Object::toString).collect(Collectors.joining(",")));
+        return sb.toString();
     }
 }
